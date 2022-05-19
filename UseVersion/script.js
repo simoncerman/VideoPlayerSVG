ab1b3d800e8e05633054c6d88cdc40cdb63b6cc7
simoncerman <mineropka34@gmail.com>
2022-05-19 18:05:19 +0200

    Code cleaning

--- UseVersion/script.js
+++ UseVersion/script.js
@@ -3,25 +3,19 @@ class AnaliticVideoPlayer {
    * @param {JSON} dataSet
    */
   constructor(dataSet) {
-    //load videoPlayer (works like holder for all elements)
     this.videoPlayer = document.getElementById("video-player");
     this.video = document.getElementById("main-video");
 
-    //for editing width and moving curve
     this.coverL;
     this.coverHolder;
-
-    //for time handling
     this.interval;
 
     this.strokePaths = [];
-    this.legendTexts = [];
+    this.legendTexts = [];    
+    this.movingDots = [];
 
     //use pathGenerator.js
     this.pathHandler = new pathGenerator();
-
-    //dots moving in time
-    this.dataDots = [];
 
     //after loading metadata of video
     this.video.addEventListener("loadedmetadata", () => {
@@ -37,56 +31,65 @@ class AnaliticVideoPlayer {
    * @param {*} dataSet
    */
   afterLoad(dataSet) {
+    //creating default svgs
     let svgAfter = this.prepareSVG(this.vidWidth, this.vidHeight);
     let svgBefore = this.prepareSVG(this.vidWidth, this.vidHeight);
+    svgAfter.id = "svgAfter";
     svgBefore.id = "svgBefore";
 
-    let defs = this.prepareGradients(dataSet, false, 0);
+    //gradients
+    let defs = this.prepareGradients(dataSet, false);
     svgAfter.appendChild(defs);
-
     let defs2 = this.prepareGradients(dataSet, true, dataSet.length);
     svgBefore.appendChild(defs2);
 
-    [svgAfter, svgBefore].forEach((svg) => {
-      let strokes = [];
-      let filled = [];
-      dataSet.map((dataRow) => {
-        let points = this.prepareData(
-          dataRow.data,
-          this.vidWidth,
-          this.vidHeight
-        );
-        let strokePath = this.generateStroke(points, dataRow.color);
-        if (svg == svgBefore) {
-          strokePath.setAttributeNS(null, "stroke", "gray");
-          this.strokePaths.push(strokePath);
-        }
-        let gradientID = dataSet.indexOf(dataRow);
-        if (svg == svgBefore) {
-          gradientID += dataSet.length;
-        }
-        let fillPath = this.generateFill(
-          points,
-          dataRow.color,
-          this.vidHeight,
-          this.vidWidth,
-          gradientID
-        );
-        strokes.push(strokePath);
-        filled.push(fillPath);
-      });
-      //set strokes and filled areas into svg in correct order => stroke curves must be on top
-      [...filled, ...strokes].forEach((element) => {
-        svg.append(element);
-      });
-      svgBefore.style.opacity = "0.5";
-    });
+    svgAfter = this.prepareStrokesAndFill(svgAfter, dataSet);
+    svgBefore = this.prepareStrokesAndFill(svgBefore, dataSet, true);
+
     this.svgBefore = svgBefore;
-    this.prepareCover(this.vidWidth, this.vidHeight, svgAfter, svgBefore);
+    this.prepareCover(svgAfter, svgBefore);
 
     //creates dots and moving line on top
     this.prepareOverlay(dataSet);
     this.prepareListeners();
+  }
+
+  prepareStrokesAndFill(svg, dataSet, svgBefore = false) {
+    let strokes = [];
+    let filled = [];
+    dataSet.map((dataRow) => {
+      let points = this.prepareData(
+        dataRow.data,
+        this.vidWidth,
+        this.vidHeight
+      );
+      //stroke curve
+      let strokePath = this.generateStroke(points, dataRow.color);
+      if (svgBefore) {
+        strokePath.setAttributeNS(null, "stroke", "gray");
+        this.strokePaths.push(strokePath);
+      }
+      //fill curve
+      let gradientID = dataSet.indexOf(dataRow);
+      if (svgBefore) {
+        gradientID += dataSet.length;
+      }
+      let fillPath = this.generateFill(
+        points,
+        dataRow.color,
+        this.vidHeight,
+        this.vidWidth,
+        gradientID
+      );
+      strokes.push(strokePath);
+      filled.push(fillPath);
+    });
+    //set strokes and filled areas into svg in correct order => stroke curves must be on top
+    [...filled, ...strokes].forEach((element) => {
+      svg.append(element);
+    });
+    if (svgBefore) svg.style.opacity = "0.5";
+    return svg;
   }
 
   /**
@@ -95,7 +98,7 @@ class AnaliticVideoPlayer {
    * @param {boolean} gray
    * @returns <defs>
    */
-  prepareGradients(dataSet, gray = false, startingID) {
+  prepareGradients(dataSet, gray = false, startingID = 0) {
     let defs = document.createElement("defs");
     for (let y = 0; y < dataSet.length; y++) {
       const dataRow = dataSet[y];
@@ -104,43 +107,49 @@ class AnaliticVideoPlayer {
         "linearGradient"
       );
       let id = y + startingID;
-      gradient.setAttributeNS(null, "id", "gradient" + id);
-      gradient.setAttributeNS(null, "x1", "0%");
-      gradient.setAttributeNS(null, "y1", "0%");
-      gradient.setAttributeNS(null, "x2", "0%");
-      gradient.setAttributeNS(null, "y2", "100%");
+      this.setMultipleAtributes(gradient, {
+        id: "gradient" + id,
+        x1: "0%",
+        y1: "0%",
+        x2: "0%",
+        y2: "100%",
+      });
 
       let stop1 = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "stop"
       );
-      stop1.setAttributeNS(null, "offset", "0%");
+      this.setMultipleAtributes(stop1, {
+        offset: "0%",
+        "stop-opacity": "80%",
+      });
       if (gray) {
-        stop1.setAttributeNS(null, "stop-color", "gray");
+        stop1.setAttribute("stop-color", "gray");
       } else {
-        stop1.setAttributeNS(null, "stop-color", dataRow.color);
+        stop1.setAttribute("stop-color", dataRow.color);
       }
-      stop1.setAttributeNS(null, "stop-opacity", "80%");
 
       let stop2 = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "stop"
       );
-      stop2.setAttributeNS(null, "offset", "100%");
-      stop2.setAttributeNS(null, "stop-opacity", "0%");
+      this.setMultipleAtributes(stop2, {
+        offset: "100%",
+        "stop-opacity": "0%",
+      });
       if (gray) {
-        stop2.setAttributeNS(null, "stop-color", "gray");
+        stop2.setAttribute("stop-color", "gray");
       } else {
-        stop2.setAttributeNS(null, "stop-color", dataRow.color);
+        stop2.setAttribute("stop-color", dataRow.color);
       }
 
       gradient.appendChild(stop1);
       gradient.appendChild(stop2);
-
       defs.appendChild(gradient);
     }
     return defs;
   }
+
   /**
    * Create svg element with specific w|h
    * @param {*} width
@@ -149,18 +158,20 @@ class AnaliticVideoPlayer {
    */
   prepareSVG(width, height) {
     let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.setAttribute("viewBox", `0 0 ${width} ${height}`);
-    svg.setAttribute("version", "1.1");
-    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    svg.setAttribute("height", "100%");
+    this.setMultipleAtributes(svg, {
+      viewBox: `0 0 ${width} ${height}`,
+      version: "1.1",
+      xmlns: "http://www.w3.org/2000/svg",
+      height: "100%",
+    });
     return svg;
   }
+
   /**
    * Recalculate data to points
    * @param {*} data
    * @param {*} vidWidth
    * @param {*} vidHeight
-   * @returns
    */
   prepareData(data, vidWidth, vidHeight) {
     let points = [];
@@ -172,6 +183,7 @@ class AnaliticVideoPlayer {
     }
     return points;
   }
+
   /**
    * Prepare cover wich contains left and right
    * moving svgs and other important elements
@@ -180,20 +192,18 @@ class AnaliticVideoPlayer {
    * @param {*} svgTo
    * @param {*} svgFrom
    */
-  prepareCover(width, height, svgAfter, svgBefore) {
+  prepareCover(svgAfter, svgBefore) {
     let coverHolder = document.createElement("div");
     let coverL = document.createElement("div");
     let coverR = document.createElement("div");
+
+    coverHolder.classList.add("coverHolder");
+    coverL.classList.add("coverL");
+    coverR.classList.add("coverR");
+
     coverHolder.appendChild(coverL);
     coverHolder.appendChild(coverR);
-
-    coverHolder.style.padding = this.padding + "px";
-    coverHolder.classList.add("coverHolder");
-
-    coverL.classList.add("coverL");
     coverL.appendChild(svgAfter);
-
-    coverR.classList.add("coverR");
     coverR.appendChild(svgBefore);
 
     this.coverL = coverL;
@@ -220,7 +230,7 @@ class AnaliticVideoPlayer {
       if (((100 - dataRow.data[0]) / 100) * this.vidHeight < dotMax) {
         dotMax = ((100 - dataRow.data[0]) / 100) * this.vidHeight;
       }
-      let dot = this.generateDot(
+      let dot = this.generateMovingGroup(
         dataRow.color,
         0,
         ((100 - dataRow.data[0]) / 100) * this.vidHeight,
@@ -266,19 +276,19 @@ class AnaliticVideoPlayer {
       x2: "0",
       y2: startingHeigth,
       stroke: "white",
-      "stroke-width": "2",
-      "stroke-dasharray": "3",
+      "stroke-width": "1",
     });
     return line;
   }
-  /**
-   * Function responsible for generating dots on default position with specific color
-   * @param {*} color
-   * @param {*} posX
-   * @param {*} posY
-   * @returns
-   */
-  generateDot(color, posX, posY, txt) {
+/**
+ * Function responsible for generating dots on default position with specific color
+ * @param {*} color 
+ * @param {*} posX 
+ * @param {*} posY 
+ * @param {*} txt 
+ * @returns 
+ */
+  generateMovingGroup(color, posX, posY, txt) {
     let circleGroup = document.createElementNS(
       "http://www.w3.org/2000/svg",
       "g"
@@ -295,6 +305,7 @@ class AnaliticVideoPlayer {
       "http://www.w3.org/2000/svg",
       "text"
     );
+
     this.setMultipleAtributes(outerCircle, {
       r: "9",
       fill: "white",
@@ -312,7 +323,7 @@ class AnaliticVideoPlayer {
     });
 
     legendText.innerHTML = txt;
-    legendText.style.visibility = "hidden";
+    legendText.style.visibility = "hidden"; //default visibility
     this.legendTexts.push(legendText);
 
     circleGroup.setAttribute("transform", `translate(${posX},${posY})`);
@@ -321,7 +332,7 @@ class AnaliticVideoPlayer {
     circleGroup.appendChild(innerCircle);
     circleGroup.appendChild(legendText);
 
-    this.dataDots.push(circleGroup);
+    this.movingDots.push(circleGroup);
     return circleGroup;
   }
   /**
@@ -339,6 +350,7 @@ class AnaliticVideoPlayer {
     path.setAttributeNS(null, "fill", "none");
     return path;
   }
+  
   /**
    * Responsible for generating filled areas
    * @param {*} points
@@ -360,28 +372,21 @@ class AnaliticVideoPlayer {
   }
 
   /**
+   * !This is magic script dont touch it
    * Responsible for calculating point y point from x and path
    * @param {*} path
    * @param {*} x
-   * @returns
+   * @returns y
    */
   findY(path, x) {
     var pathLength = path.getTotalLength();
     var start = 0;
     var end = pathLength;
     var target = (start + end) / 2;
-
-    // Ensure that x is within the range of the path
     x = Math.max(x, path.getPointAtLength(0).x);
     x = Math.min(x, path.getPointAtLength(pathLength).x);
-
-    // Walk along the path using binary search
-    // to locate the point with the supplied x value
     while (target >= start && target <= pathLength) {
       var pos = path.getPointAtLength(target);
-
-      // use a threshold instead of strict equality
-      // to handle javascript floating point precision
       if (Math.abs(pos.x - x) < 0.001) {
         return pos.y;
       } else if (pos.x > x) {
@@ -401,9 +406,14 @@ class AnaliticVideoPlayer {
     //change of left cover widht
     this.coverL.style.width = 100 * (percents * 1000) + "%";
     //updating dots
-    this.updateDots(percents);
+    this.updateMovingGroup(percents);
   }
-  updateDots(percents) {
+
+  /**
+   * Updating moving group position by line pos
+   * @param {*} percents 
+   */
+  updateMovingGroup(percents) {
     let maxDotPos = this.vidHeight;
     for (let i = 0; i < this.strokePaths.length; i++) {
       let path = this.strokePaths[i];
@@ -414,13 +424,14 @@ class AnaliticVideoPlayer {
       if (pos.y < maxDotPos) {
         maxDotPos = parseFloat(pos.y);
       }
-      this.dataDots[i].setAttribute(
+      this.movingDots[i].setAttribute(
         "transform",
         `translate(${pos.x},${pos.y})`
       );
     }
     this.updateVerticalLine(percents, maxDotPos);
   }
+
   updateVerticalLine(percents, maxDotPos) {
     this.line.setAttribute("x1", this.vidWidth * percents * 1000);
     this.line.setAttribute("x2", this.vidWidth * percents * 1000);
@@ -450,6 +461,7 @@ class AnaliticVideoPlayer {
       this.updateCover(this.video.currentTime / this.vidDuration);
     });
   }
+  
   /**
    *
    * @param {*} element
@@ -471,3 +483,18 @@ class AnaliticVideoPlayer {
     return element;
   }
 }
+
+let dataSet = [
+  {
+    color: "#1BE7FF",
+    data: [70, 50, 76, 90, 68, 50, 59, 60, 49, 40, 30, 31, 28, 10],
+    legendText: "Brand",
+  },
+  {
+    color: "#13EFC9",
+    data: [15, 21, 60, 40, 50, 30, 50, 20, 70, 30, 40, 60, 80, 90],
+    legendText: "Need",
+  },
+];
+
+let playerHandler = new AnaliticVideoPlayer(dataSet);
